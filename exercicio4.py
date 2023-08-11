dec4f0125de251291ecbd3bc41d60cb7b2125ecf
Logon Aluno <logonrmlocal@fiap.com.br>
2023-08-11 11:43:54 -0300

    exercicio

--- exercicio4.py
+++ exercicio4.py
@@ -3,3 +3,13 @@ assunto e corpo. O parâmetro assunto deve ter um valor padrão de "Sem assunto"
 O parâmetro corpo deve ter um valor padrão de uma string vazia. A função deve 
 imprimir as informações formatadas. Inclua uma docstring que explique brevemente 
 o propósito da função. '''
+
+def enviar_email(destinatario, assunto='Sem assunto', corpo=''):
+    print(f'Destinatario: {destinatario}')
+    print(f'Assunto: {assunto}')
+    print(f'Corpo: {corpo}')
+
+enviar_email('annylinda@email.com.br')
+enviar_email('annylinda@email.com.br', 'urgente')
+enviar_email('annylinda@email.com.br', corpo='conteudo do email')
+
